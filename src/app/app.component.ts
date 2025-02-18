cb70c2862c90c6e8d2834db464887a9ebaf00ad6
guilhermelins7 <guilherme7lins@gmail.com>
2025-02-18 20:37:16 -0300

    feat: Criado tela inicial do app de tarefas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tarefa } from './tarefas';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'TODOapp';
+
+  arrayDeTarefas: Tarefa[] = [];
+
+  constructor() {
+    this.READ_tarefas();
+  }
+
+  CREATE_tarefa(descricaoNovaTarefa: string){
+    var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.arrayDeTarefas.unshift(novaTarefa);
+  }
+
+  READ_tarefas() {
+    this.arrayDeTarefas = [
+      new Tarefa("Estudar Frameworks WEB", false),
+      new Tarefa("Comer Pizza", false),
+      new Tarefa("Ajudar meus pais", false)
+    ];
+  }
 }
