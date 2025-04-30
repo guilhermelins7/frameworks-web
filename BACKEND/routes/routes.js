b22727f51ec8b729a47f4d614a035c0f6788d070
guilhermelins7 <guilherme7lins@gmail.com>
2025-04-30 20:26:39 -0300

    feat: implementar autenticação com JWT

--- BACKEND/routes/routes.js
+++ BACKEND/routes/routes.js
@@ -1,55 +1,79 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 module.exports = router;
 const modeloTarefa = require('../models/tarefa');
 
 router.post('/post', async (req, res) => {
     const objetoTarefa = new modeloTarefa({
-    descricao: req.body.descricao,
-    statusRealizada: req.body.statusRealizada
-    })
+        descricao: req.body.descricao,
+        statusRealizada: req.body.statusRealizada
+    });
     try {
-    const tarefaSalva = await objetoTarefa.save();
-    res.status(200).json(tarefaSalva)
+        const tarefaSalva = await objetoTarefa.save();
+        res.status(200).json(tarefaSalva);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-    catch (error) {
-    res.status(400).json({ message: error.message })
-    }
-   })
+});
 
-   router.get('/getAll', async (req, res) => {
+router.get('/getAll', verificaJWT, async (req, res) => {
     try {
-    const resultados = await modeloTarefa.find();
-    res.json(resultados)
+        const resultados = await modeloTarefa.find();
+        res.json(resultados);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
-    catch (error) {
-    res.status(500).json({ message: error.message })
-    }
-   })
+});
 
-   router.delete('/delete/:id', async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
     try {
-    const resultado = await modeloTarefa.findByIdAndDelete(req.params.id)
-    res.json(resultado)
+        const resultado = await modeloTarefa.findByIdAndDelete(req.params.id);
+        res.json(resultado);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-    catch (error) {
-    res.status(400).json({ message: error.message })
-    }
-   })
+});
 
-   router.patch('/update/:id', async (req, res) => {
+router.patch('/update/:id', async (req, res) => {
     try {
-    const id = req.params.id;
-    const novaTarefa = req.body;
-    const options = { new: true };
-    const result = await modeloTarefa.findByIdAndUpdate(
-    id, novaTarefa, options
-    )
-    res.json(result)
+        const id = req.params.id;
+        const novaTarefa = req.body;
+        const options = { new: true };
+        const result = await modeloTarefa.findByIdAndUpdate(
+            id, novaTarefa, options
+        );
+        res.json(result);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-    catch (error) {
-    res.status(400).json({ message: error.message })
+});
+
+//Autorizacao
+function verificaUsuarioSenha(req, res, next) {
+    if (req.body.nome !== 'branqs' || req.body.senha !== '1234') {
+        return res.status(401).json({ auth: false, message: 'Usuario ou Senha incorreta' });
     }
-   })
-   
-   
+    next();
+}
+
+// Autenticacao
+var jwt = require('jsonwebtoken');
+router.post('/login', async (req, res) => {
+    if (req.body.nome === 'branqs' && req.body.senha === '1234') {
+        const token = jwt.sign({ id: req.body.nome }, 'segredo', { expiresIn: 300 });
+        return res.json({ auth: true, token: token });
+    }
+    res.status(500).json({ message: 'Login invalido!' });
+});
+
+//Nova forma de Autorizacao
+function verificaJWT(req, res, next) {
+    const token = req.headers['id-token'];
+    if (!token) return res.status(401).json({
+        auth: false, message: 'Token nao fornecido'
+    });
+    jwt.verify(token, 'segredo', function (err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Falha !' });
+        next();
+    });
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Tarefa } from './tarefa';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +12,8 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'TODOapp';
   apiURL: string = 'http://localhost:3000';
+  usuarioLogado = false;
+  tokenJWT = '{ "token":""}';
 
   constructor(private service: HttpClient) {
     this.READ_tarefas();
@@ -19,31 +22,57 @@ export class AppComponent {
   arrayDeTarefas: Tarefa[] = [];
 
   READ_tarefas() {
-    this.service.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
-      resultado => this.arrayDeTarefas=resultado);
+    const idToken = new HttpHeaders().set("id-token", JSON.parse(this.tokenJWT).token);
+    this.service.get<Tarefa[]>(`${this.apiURL}/api/getAll`, { 'headers': idToken }).subscribe(
+      (resultado) => { 
+        this.arrayDeTarefas = resultado; 
+        this.usuarioLogado = true 
+      },
+      (error) => { 
+        this.usuarioLogado = false 
+      }
+    );
   }
 
   CREATE_tarefa(descricaoNovaTarefa: string) {
     var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
     this.service.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
-      resultado => { console.log(resultado); this.READ_tarefas(); });
+      resultado => { 
+        console.log(resultado); 
+        this.READ_tarefas(); 
+      }
+    );
   }
 
   DELETE_tarefa(tarefaASerRemovida: Tarefa) {
     var indice = this.arrayDeTarefas.indexOf(tarefaASerRemovida);
     var id = this.arrayDeTarefas[indice]._id;
     this.service.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
-    resultado => {
-      console.log(resultado); 
-      this.READ_tarefas(); 
-    });
+      resultado => {
+        console.log(resultado);
+        this.READ_tarefas();
+      }
+    );
   }
 
   UPDATE_tarefa(tarefaAserModificada: Tarefa) {
     var indice = this.arrayDeTarefas.indexOf(tarefaAserModificada);
     var id = this.arrayDeTarefas[indice]._id;
-    this.service.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
-    tarefaAserModificada).subscribe(
-    resultado => { console.log(resultado); this.READ_tarefas(); });
-   }
+    this.service.patch<Tarefa>(`${this.apiURL}/api/update/${id}`, tarefaAserModificada).subscribe(
+      resultado => { 
+        console.log(resultado); 
+        this.READ_tarefas(); 
+      }
+    );
+  }
+
+  login(username: string, password: string) {
+    var credenciais = { "nome": username, "senha": password };
+    this.service.post(`${this.apiURL}/api/login`, credenciais).subscribe(
+      resultado => {
+        this.tokenJWT = JSON.stringify(resultado);
+        this.READ_tarefas();
+      }
+    );
+  }
 }
